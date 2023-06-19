47efba6ae70f5b16506bb11481dff140ba26a334
cloudcoke <lms4296@gmail.com>
2023-06-19 16:41:30 +0900

    test

--- back/src/app.controller.spec.ts
+++ back/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Success to Health Check"', () => {
+      expect(appController.getHello()).toBe('Success to Health Check');
     });
   });
 });
